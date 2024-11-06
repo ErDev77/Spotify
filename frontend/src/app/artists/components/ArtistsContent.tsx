0ed2a3dd468618a93d4c983196fa5bf93ff1e902
ErDev77 <erokazaryan705@gmail.com>
2024-11-06 22:32:18 +0400

    error fix

--- frontend/src/app/artists/components/ArtistsContent.tsx
+++ frontend/src/app/artists/components/ArtistsContent.tsx
@@ -1,22 +1,29 @@
-"use client";
+'use client'
 
 import { useRouter } from 'next/navigation'
 import { useUser } from '@/hooks/useUser'
 import { useEffect } from 'react'
 import useAuthModal from '@/hooks/useAuthModal'
-const ArtistsContent = () => {
-    	const { isLoading, user } = useUser()
-        const router = useRouter()
-        const authModal = useAuthModal()
-        if(!user) {
-            return authModal.onOpen();
-        }
 
-		 useEffect(() => {
+const ArtistsContent = () => {
+	const { isLoading, user } = useUser()
+	const router = useRouter()
+	const authModal = useAuthModal()
+
+	useEffect(() => {
 		if (!isLoading && !user) {
-				router.replace('/')
-		 	}
-		}, [isLoading, user, router])
+			authModal.onOpen() 
+		}
+	}, [isLoading, user, authModal])
+
+	useEffect(() => {
+		if (!isLoading && !user) {
+			router.replace('/') 
+		}
+	}, [isLoading, user, router])
+
+
+	return null 
 }
 
-export default ArtistsContent;
+export default ArtistsContent
